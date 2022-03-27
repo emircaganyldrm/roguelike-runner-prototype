0a63c31da948cdec3b571ec0d6012b00501a7eaf
emircagan <emircagany@gmail.com>
2022-03-27 15:41:36 +0300

    Revert "Revert "Update DropSpawner.cs""
    This reverts commit 503e7e92a8225f2a178f9aa068ae2d220dd2b627.

--- Assets/Scripts/DropSpawner.cs
+++ Assets/Scripts/DropSpawner.cs
@@ -6,6 +6,7 @@ public class DropSpawner : MonoBehaviour
 {
     [Header("Objects To Drop")]
     public List<GameObject> Drops;
+    
     public void Drop(){
         for (int i = 0; i < Drops.Count; i++)
         {
